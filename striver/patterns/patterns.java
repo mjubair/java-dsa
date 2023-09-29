5fd3bc9250a9986346707ce4963f1ef9f1fb0aec
Mohamed Jubair <amzubair@live.com>
2023-09-29 12:14:33 +0000

    Alternate approach to pattern12

--- striver/patterns/patterns.java
+++ striver/patterns/patterns.java
@@ -151,9 +151,12 @@ public class patterns {
             // for(int j = 0; j < N - i - 1; j++) {
             //     System.out.print("_ ");
             // } 
-            for(int j = 0; j <= i; j++) {
-                System.out.print(i + 1 - j + " ");
+            for(int j = i+1; j > 0; j--) {
+                System.out.print(j + " ");
             }
+            // for(int j = 0; j <= i; j++) {
+            //     System.out.print(i + 1 - j + " ");
+            // }
             System.out.println();
         }
     }
