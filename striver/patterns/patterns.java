3dee76f52bdb459047b90157796f5ac5a1cf52a9
Mohamed Jubair <amzubair@live.com>
2023-09-30 07:38:47 +0000

    solve striver pattern problem 19

--- striver/patterns/patterns.java
+++ striver/patterns/patterns.java
@@ -9,7 +9,7 @@ public class patterns {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int N = scn.nextInt();
-        pattern17(N);
+        pattern19(N);
         scn.close();
     }
 
@@ -211,6 +211,33 @@ public class patterns {
             }
             for(int j = 0; j < i; j++) {
                 System.out.print((char)('A' + i - j - 1)  + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern19(int N) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N-i; j++) {
+                System.out.print("* ");
+            }
+            for(int j = 0; j < i*2; j++) {
+                System.out.print("  ");
+            }
+            for(int j = 0; j < N-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            for(int j = 0; j < 2*(N-i-1); j++) {
+                System.out.print("  ");
+            }
+            for(int j = 0; j < i+1; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
